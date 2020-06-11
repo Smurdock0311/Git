d67f621d0f11856596601233fd048d8eff5eef62
Steven murdock <smurdockstudent@gmail.com>
2020-06-11 12:34:32 -0600

    Add project files.

---
+++ Git/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a Git backup test");
+            Console.ReadLine();
+        }
+    }
+}
